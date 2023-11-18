7bdfbecf1b0f47d280e3dc3c1c2c3165006a3451
EmSturm <emsturmova@gmail.com>
2023-11-18 11:41:01 +0100

    Příprava Header

--- hotel_stribrina/src/index.jsx
+++ hotel_stribrina/src/index.jsx
@@ -1,7 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import { HomePage } from './pages/HomePage';
 import './global.css';
+import { Header } from './çomponents/Header';
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
 
 createRoot(
   document.querySelector('#app'),
 ).render(<HomePage />);
+
--- hotel_stribrina/src/çomponents/Header/index.jsx
+++ hotel_stribrina/src/çomponents/Header/index.jsx
@@ -5,9 +5,11 @@ export const Header = () => {
         <div class="banner">
     <div class="banner__stripe">
       <div class="container">
-        <h1>Template</h1>
+        <h1>Hotel Stříbrava</h1>
         <p class="lead">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, sequi.
+          Rodinný hotel v malebné krajině Českého středohoří.
+          <img src="../images/hotel.jpg" alt="" />
+          
         </p>
       </div>
     </div>
