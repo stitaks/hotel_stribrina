26c0d0ef6208c78fbcb69bcc36fadfe55c27b579
EmSturm <emsturmova@gmail.com>
2023-11-18 10:48:31 +0100

    podklady API

--- hotel_stribrina/src/çomponents/Header/index.jsx
+++ hotel_stribrina/src/çomponents/Header/index.jsx
@@ -1,1 +1,16 @@
-import './style.css;
+import './style.css';
+
+export const Header = () => {
+    return (
+        <div class="banner">
+    <div class="banner__stripe">
+      <div class="container">
+        <h1>Template</h1>
+        <p class="lead">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, sequi.
+        </p>
+      </div>
+    </div>
+  </div>
+    )
+}
